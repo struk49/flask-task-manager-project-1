4845aa2141de12e959f155208156a1d00971c5ad
struk49 <>
2023-08-06 10:42:53 +0000

    Add signout functionality

--- app.py
+++ app.py
@@ -81,7 +81,19 @@ def profile(username):
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("signin"))
+
+
+@app.route("/signout")
+def signout():
+    # remove user from session cookies
+    flash("you have been signed out")
+    session.pop("user")
+    return redirect(url_for("signin"))
 
 
 if __name__ == "__main__":
