42b8e8f4b6336e07bdd5f7bae867d1d49eae2278
struk49 <>
2023-08-09 10:03:13 +0000

    i have no idea

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
-    tasks = list(mongo.db.tasks.find())
+    tasks = list(mongo.db.tasks.find_one())
     return render_template("tasks.html", tasks=tasks)
 
 
@@ -94,7 +94,7 @@ def profile(username):
 @app.route("/signout")
 def signout():
     # remove user from session cookie
-    flash("You have been signed out")
+    flash("You have been logged out")
     session.pop("user")
     return redirect(url_for("signin"))
 
@@ -121,6 +121,25 @@ def add_task():
 
 @app.route("/edit_task/<task_id>", methods=["GET", "POST"])
 def edit_task(task_id):
+    if request.method == "POST":
+        is_urgent = "on" if request.form.get("is_urgent") else "off"
+        submit = {
+            "category_name": request.form.get("category_name"),
+            "task_name": request.form.get("task_name"),
+            "task_description": request.form.get("task_description"),
+            "is_urgent": is_urgent,
+            "due_date": request.form.get("due_date"),
+            "created_by": session["user"]
+        }
+        mongo.db.tasks.update({"_id": ObjectId(task_id)}, submit)
+        flash("Task Successfully Updated")
+
+# coll.update_one
+#    {"nationality": "american"},
+#    {"$set": {"hair_color": "maroon"}}
+# )
+# documents = coll.find({"nationality": "american"})
+
     task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("edit_task.html", task=task, categories=categories)
