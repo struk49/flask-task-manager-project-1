64b9f963de11c80f858f17424be1e8963e180910
struk49 <>
2023-08-16 07:02:00 +0000

    Add delete category function

--- app.py
+++ app.py
@@ -177,10 +177,14 @@ def edit_category(category_id):
         flash("Category successfully updated")
         return redirect(url_for('get_categories'))
 
-
     category = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
     return render_template("edit_category.html", category=category)
 
+@app.route("/delete_category/<category_id>")
+def delete_category(category_id):
+    mongo.db.categories.delete_one({"_id": ObjectId(category_id)})
+    flash("Category successfully deleted")
+    return redirect(url_for("get_categories"))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
